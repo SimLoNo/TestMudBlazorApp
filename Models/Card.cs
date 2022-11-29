4acf4d7b0e6c900a522960c3edcf4b868bbb6f8f
Simon Nørregaard <70365083+SimLoNo@users.noreply.github.com>
2022-11-29 10:56:18 +0100

    Did stuff

--- Models/Card.cs
+++ Models/Card.cs
@@ -3,10 +3,10 @@
 	public class Card
 	{
 		public int ?CardId { get; set; }
-		public int UserId { get; set; } = 0;
-		public string Title { get; set; }
-		public string CardDescription { get; set; }
-		public string CardStatus { get; set; }
+		public int UserId { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string CardDescription { get; set; } = string.Empty;
+		public string CardStatus { get; set; } = string.Empty;
 		public User ?AssignedUser { get; set; }
 		public Attachment[] ?Attachment { get; set; }
 
---
+++ Models/Column.cs
@@ -0,0 +1,17 @@
+﻿namespace TestMudBlazorApplication.Models
+{
+    public class Column
+    {
+        public int ColumnId { get; set; }
+        public string ColumnName { get; set; } = string.Empty;
+        public bool NewTaskOpen { get; set; } = false;
+        public int ?Order { get; set; }
+
+        public Column(int columnId, string columnName, bool newTaskOpen)
+        {
+            ColumnId= columnId;
+			ColumnName = columnName;
+            NewTaskOpen= newTaskOpen;
+        }
+    }
+}
